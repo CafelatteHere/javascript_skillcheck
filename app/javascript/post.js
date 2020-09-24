13337ca50240197278f4e8cfa3d7a133314ca5bf
Latteholic <cafelatte@gmail.com>
2020-09-24 22:18:22 +0900

    JavaScript練習問題: round2

--- app/javascript/post.js
+++ app/javascript/post.js
@@ -2,7 +2,28 @@ function post() {
   const inputForm = document.getElementById("article_text");
   const submitBtn = document.getElementById("submit_btn");
   submitBtn.addEventListener('click', (e) => {
-    console.log(inputForm.value);
+    const XHR = new XMLHttpRequest();
+    const formData = new FormData(document.getElementById("new_article"));
+    XHR.open('POST', '/articles', true);
+    XHR.responseType = 'json';
+    XHR.send(formData);
+    
+
+    XHR.onload = () => {
+      if (XHR.status != 200) {
+        alert(`Произошла ошибка ${XHR.status}: ${XHR.statusText}`);
+      }
+      const inputForm = document.getElementById('article_text');
+      const contents = document.getElementById('contents_area');
+      const article = XHR.response.article;
+      const HTML = `
+        <div class="article">
+          ${article.text}
+        </div>`
+      contents.insertAdjacentHTML('afterBegin', HTML);
+      inputForm.value = "";
+    };
+
     e.preventDefault();
   });
 }
