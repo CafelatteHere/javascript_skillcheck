1ad338e7f10d5ad98865ee2aba14cecff9a336e6
Latteholic <cafelatte@gmail.com>
2020-10-02 23:08:54 +0900

    練習: rount6, the last one!

--- app/javascript/post.js
+++ app/javascript/post.js
@@ -2,9 +2,28 @@ function post() {
   const inputForm = document.getElementById("article_text");
   const submitBtn = document.getElementById("submit_btn");
   submitBtn.addEventListener('click', (e) => {
-    console.log(inputForm.value);
+    const XHR = new XMLHttpRequest();
+    const formData = new FormData(document.getElementById("new_article"));
+    XHR.open("POST", "/articles", true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+    
+    XHR.onload = () => {
+      if (XHR.status != 200) {
+        alert(`${XHR.status}: ${XHR.statusText}`);
+      }
+      const item = XHR.response.article;
+      const contentsArea = document.getElementById("contents_area");
+      const HTML = `<div class="article">
+          ${item.text}
+        </div>`;
+      contentsArea.insertAdjacentHTML("afterbegin", HTML);
+      inputForm.value = "";
+    };
+    
     e.preventDefault();
   });
+  
 }
 
 window.addEventListener('load', post);
